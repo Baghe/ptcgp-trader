f8620049249e620528335f13fbf636cf7f76a028
baghe <baghega1@gmail.com>
2025-01-30 18:20:06 +0100

    v2.1

--- src/App/Sections/MyTrades.tsx
+++ src/App/Sections/MyTrades.tsx
@@ -93,111 +93,115 @@ export const MyTrades: FC = () => {
     <>
       <div className="container py-3">
         <div className="text-primary h5">My open trades{Object.keys(Trades).length > 0 && " (" + Object.keys(Trades).length + ")"}</div>
-        <div className="card mb-4">
-          <div className="card-body">
-            {Object.keys(Trades).length === 0 ? (
+        {Object.keys(Trades).length === 0 ? (
+          <div className="card mb-4">
+            <div className="card-body">
               <div className="text-muted small">No trades</div>
-            ) : (
-              <div className="d-flex flex-column gap-2">
-                {Object.keys(Trades).map((IdTrade: string) => {
-                  const Trade = Trades[IdTrade] as any;
-                  return (
-                    <div key={IdTrade} className="rounded-2 overflow-hidden p-2" style={{ backgroundColor: "#17212b" }}>
-                      <div className="mb-2">
-                        {Trade.Status === "open" && (
-                          <span className="fw-bold fs-6 text-primary">
-                            <div className="spinner-border spinner-border-sm text-primary me-2" role="status" />
-                            Trade open
-                          </span>
-                        )}
-                        {Trade.Status === "accepted" && (
-                          <span className="fw-bold fs-6 text-success">
-                            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 256 256" className="me-2">
-                              <path fill="currentColor" d="M220.69 100.17a12 12 0 0 0-9.85 13.83a85 85 0 0 1 1.16 14a83.57 83.57 0 0 1-18 51.94a83.5 83.5 0 0 0-29-23.42a52 52 0 1 0-74 0a83.5 83.5 0 0 0-29 23.42A83.94 83.94 0 0 1 128 44a85 85 0 0 1 14 1.16a12 12 0 0 0 4-23.67A108.1 108.1 0 0 0 20 128a108 108 0 0 0 216 0a109 109 0 0 0-1.49-18a12 12 0 0 0-13.82-9.83M100 120a28 28 0 1 1 28 28a28 28 0 0 1-28-28m-20.43 76.57a60 60 0 0 1 96.86 0a83.72 83.72 0 0 1-96.86 0M240.49 48.49l-32 32a12 12 0 0 1-17 0l-16-16a12 12 0 0 1 17-17L200 55l23.51-23.52a12 12 0 1 1 17 17Z"></path>
-                            </svg>
-                            Trade offer accepted
-                          </span>
-                        )}
-                      </div>
-                      <div className="d-flex">
-                        <img src={"https://static.dotgg.gg/pokepocket/card/" + Trade.IdCard + ".webp"} alt="" style={{ height: 96 }} />
-                        <div className="flex-grow-1 ms-2">
-                          <div className="badge bg-primary">{Cards[Trade.IdCard][1]}</div>
-                          <div className="fw-bold text-truncate">{Cards[Trade.IdCard][0]}</div>
-                          <div className="d-flex flex-wrap gap-1">
+            </div>
+          </div>
+        ) : (
+          <div className="d-flex flex-column gap-2 mb-4">
+            {Object.keys(Trades).map((IdTrade: string) => {
+              const Trade = Trades[IdTrade] as any;
+              return (
+                <div key={IdTrade} className="card">
+                  <div className="card-body py-2">
 
+                    <div className="mb-1">
+                      {Trade.Status === "open" && (
+                        <span className="fw-bold fs-6 text-primary">
+                          <div className="spinner-border spinner-border-sm text-primary me-2" role="status" />
+                          Trade open
+                        </span>
+                      )}
+                      {Trade.Status === "accepted" && (
+                        <span className="fw-bold fs-6 text-success">
+                          <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 256 256" className="me-2">
+                            <path fill="currentColor" d="M220.69 100.17a12 12 0 0 0-9.85 13.83a85 85 0 0 1 1.16 14a83.57 83.57 0 0 1-18 51.94a83.5 83.5 0 0 0-29-23.42a52 52 0 1 0-74 0a83.5 83.5 0 0 0-29 23.42A83.94 83.94 0 0 1 128 44a85 85 0 0 1 14 1.16a12 12 0 0 0 4-23.67A108.1 108.1 0 0 0 20 128a108 108 0 0 0 216 0a109 109 0 0 0-1.49-18a12 12 0 0 0-13.82-9.83M100 120a28 28 0 1 1 28 28a28 28 0 0 1-28-28m-20.43 76.57a60 60 0 0 1 96.86 0a83.72 83.72 0 0 1-96.86 0M240.49 48.49l-32 32a12 12 0 0 1-17 0l-16-16a12 12 0 0 1 17-17L200 55l23.51-23.52a12 12 0 1 1 17 17Z"></path>
+                          </svg>
+                          Trade offer accepted
+                        </span>
+                      )}
+                    </div>
+
+                    <div className="d-flex rounded-2 overflow-hidden p-2" style={{ backgroundColor: "#17212b" }}>
+                      <img src={"https://static.dotgg.gg/pokepocket/card/" + Trade.IdCard + ".webp"} alt="" style={{ height: 96 }} />
+                      <div className="flex-grow-1 ms-2">
+                        <div className="badge bg-primary">{Cards[Trade.IdCard][1]}</div>
+                        <div className="fw-bold text-truncate">{Cards[Trade.IdCard][0]}</div>
+                        <div className="d-flex flex-wrap gap-1">
+                          <div className='d-flex align-items-center'>
+                            <div className="fw-bold small" style={{ width: 24, height: 24 }}>
+                              <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+                                <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 17h12M4 17l3.5-3.5M4 17l3.5 3.5M7 7h13m0 0l-3.5-3.5M20 7l-3.5 3.5"></path>
+                              </svg>
+                            </div>
+                          </div>
+                          {Trade.Offers.slice(0, 3).map((IdCard: string) => {
+                            return (
+                              <img src={"https://static.dotgg.gg/pokepocket/card/" + IdCard + ".webp"} alt="" key={IdCard} style={{ height: 48 }} />
+                            )
+                          })}
+                          {Trade.Offers.length > 3 && (
                             <div className='d-flex align-items-center'>
-                              <div className="fw-bold small" style={{ width: 24, height: 24 }}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
-                                  <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 17h12M4 17l3.5-3.5M4 17l3.5 3.5M7 7h13m0 0l-3.5-3.5M20 7l-3.5 3.5"></path>
-                                </svg>
+                              <div className="fw-bold small text-nowrap" style={{ width: 24, height: 24 }}>
+                                +{Trade.Offers.length - 3}
                               </div>
                             </div>
-                            {Trade.Offers.slice(0, 3).map((IdCard: string) => {
-                              return (
-                                <img src={"https://static.dotgg.gg/pokepocket/card/" + IdCard + ".webp"} alt="" key={IdCard} style={{ height: 48 }} />
-                              )
-                            })}
-                            {Trade.Offers.length > 3 && (
-                              <div className='d-flex align-items-center'>
-                                <div className="fw-bold small" style={{ width: 24, height: 24 }}>
-                                  +{Trade.Offers.length - 3}
-                                </div>
-                              </div>
-                            )}
-                          </div>
+                          )}
                         </div>
                       </div>
+                    </div>
 
-                      {Trade.Status === "open" && (
-                        <div className="mt-2">
-                          <button className="btn btn-sm btn-danger fw-bold px-3" onClick={() => TradeCancel(IdTrade)}>
-                            Cancel trade
-                          </button>
+                    {Trade.Status === "open" && (
+                      <div className="mt-1">
+                        <button className="btn btn-sm btn-danger fw-bold px-3" onClick={() => TradeCancel(IdTrade)}>
+                          Cancel trade
+                        </button>
 
-                          {Trade.Bids.length > 0 ? (
-                            <>
-                              <div className="text-primary h6 mt-3">Trade offers received ({Trade.Bids.length})</div>
-                              <div className="d-flex flex-column gap-1 p-1">
-                                {Trade.Bids.map((Bid: any) => (
-                                  <div key={Bid.IdHash} className="card">
-                                    <div className="card-body p-2">
+                        {Trade.Bids.length > 0 ? (
+                          <>
+                            <div className="text-primary h6 mt-3 mb-1">Trade offers received ({Trade.Bids.length})</div>
+                            <div className="d-flex flex-column gap-1">
+                              {Trade.Bids.map((Bid: any) => (
+                                <div key={Bid.IdHash} className="card rounded-2" style={{ backgroundColor: "#17212b" }}>
+                                  <div className="card-body p-2">
 
-                                      <div className="d-flex align-items-center">
-                                        <img src={"https://static.dotgg.gg/pokepocket/card/" + Bid.IdCard + ".webp"} alt="" style={{ height: 31 }} className="me-2" />
-                                        <div className="flex-grow-1 lh-1">
-                                          <div className="fw-bold">{Cards[Bid.IdCard][0]}</div>
-                                          <div className="small opacity-25">{new Date(Bid.DateBid).toLocaleString()}</div>
-                                        </div>
-                                        <div className="text-nowrap d-flex">
-                                          <button className="btn btn-sm btn-danger fw-bold px-2 me-1" onClick={() => BidReject(IdTrade, Bid.IdHash)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width={21} height={21} viewBox="0 0 24 24" className='d-block m-auto'>
-                                              <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m7 7l10 10M7 17L17 7"></path>
-                                            </svg>
-                                          </button>
-                                          <button className="btn btn-sm btn-success fw-bold px-2" onClick={() => BidAccept(IdTrade, Bid.IdHash)}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width={20} height={19} viewBox="0 0 24 24" className='d-inline'>
-                                              <path fill="currentColor" d="m9.55 15.15l8.475-8.475q.3-.3.7-.3t.7.3t.3.713t-.3.712l-9.175 9.2q-.3.3-.7.3t-.7-.3L4.55 13q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3z"></path>
-                                            </svg>
-                                            <span className='d-none d-sm-inline ms-1 me-2'>Accept</span>
-                                          </button>
-                                        </div>
+                                    <div className="d-flex align-items-center">
+                                      <img src={"https://static.dotgg.gg/pokepocket/card/" + Bid.IdCard + ".webp"} alt="" style={{ height: 31 }} className="me-2" />
+                                      <div className="flex-grow-1 lh-1">
+                                        <div className="fw-bold">{Cards[Bid.IdCard][0]}</div>
+                                        <div className="small opacity-25">{new Date(Bid.DateBid).toLocaleString()}</div>
+                                      </div>
+                                      <div className="text-nowrap d-flex">
+                                        <button className="btn btn-sm btn-danger fw-bold px-2 me-1" onClick={() => BidReject(IdTrade, Bid.IdHash)}>
+                                          <svg xmlns="http://www.w3.org/2000/svg" width={21} height={21} viewBox="0 0 24 24" className='d-block m-auto'>
+                                            <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m7 7l10 10M7 17L17 7"></path>
+                                          </svg>
+                                        </button>
+                                        <button className="btn btn-sm btn-success fw-bold px-2" onClick={() => BidAccept(IdTrade, Bid.IdHash)}>
+                                          <svg xmlns="http://www.w3.org/2000/svg" width={20} height={19} viewBox="0 0 24 24" className='d-inline'>
+                                            <path fill="currentColor" d="m9.55 15.15l8.475-8.475q.3-.3.7-.3t.7.3t.3.713t-.3.712l-9.175 9.2q-.3.3-.7.3t-.7-.3L4.55 13q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3z"></path>
+                                          </svg>
+                                          <span className='d-none d-sm-inline ms-1 me-2'>Accept</span>
+                                        </button>
                                       </div>
                                     </div>
                                   </div>
-                                ))}
-                              </div>
-                            </>
-                          ) : (
-                            <div className="text-warning h6 mt-3 mb-0">No trade offers received yet</div>
-                          )}
-                        </div>
-                      )}
+                                </div>
+                              ))}
+                            </div>
+                          </>
+                        ) : (
+                          <span className="text-muted small ms-2">No trade offers received yet</span>
+                        )}
+                      </div>
+                    )}
 
-                      {Trade.Status === "accepted" && (
-                        <>
-                          <div className="text-primary h6 mt-3">Trader</div>
-
+                    {Trade.Status === "accepted" && (
+                      <>
+                        <div className="text-primary h6 mt-3 mb-1">Trader</div>
+                        <div className="rounded-2 overflow-hidden p-2" style={{ backgroundColor: "#17212b" }}>
                           {Trade.Trader.FriendName && (
                             <div className="mb-2">
                               <div className="small text-muted">Player</div>
@@ -245,15 +249,15 @@ export const MyTrades: FC = () => {
                               </button>
                             </div>
                           </div>
-                        </>
-                      )}
-                    </div>
-                  )
-                })}
-              </div>
-            )}
+                        </div>
+                      </>
+                    )}
+                  </div>
+                </div>
+              )
+            })}
           </div>
-        </div>
+        )}
 
         <div className="text-primary h6">New trade</div>
         <div className="card">
--- src/App/Sections/Trades.tsx
+++ src/App/Sections/Trades.tsx
@@ -12,10 +12,24 @@ const Cards = CardsRaw as CardsType;
 export const Trades: FC = () => {
   const { initData } = retrieveLaunchParams();
   const [Trades, setTrades] = useState(null);
+  const [TradesFiltered, setTradesFiltered] = useState<any>([]);
+  const [Search, setSearch] = useState('');
 
   useEffect(() => {
     Load()
   }, []);
+
+  useEffect(() => {
+    if (Trades) {
+      if (Search.trim().length) {
+        setTradesFiltered(Object.values(Trades).filter((Trade: any) => {
+          return Trade.Offers.some((IdCard: string) => Cards[IdCard][0].toLowerCase().includes(Search.toLowerCase()));
+        }));
+      } else {
+        setTradesFiltered(Object.values(Trades));
+      }
+    }
+  }, [Trades, Search]);
 
   function Load() {
     ApiCall("trades-get", {}, (data) => {
@@ -23,63 +37,85 @@ export const Trades: FC = () => {
     });
   }
 
-  return initData && Trades ? (
+  return initData && TradesFiltered ? (
     <div className="container py-3">
       <div className="text-primary h5">Trades</div>
-      <div className="card">
-        <div className="card-body">
 
-          <div className="card mb-4">
-            <div className="card-body">
-              {Object.keys(Trades).length === 0 ? (
-                <div className="text-muted small">No trades</div>
-              ) : (
-                <div className="d-flex flex-column gap-2">
-                  {Object.keys(Trades).map((IdTrade: string) => {
-                    const Trade = Trades[IdTrade] as any;
-                    return (
-                      <div key={IdTrade} className="rounded-2 overflow-hidden p-2" style={{ backgroundColor: "#17212b" }}>
-                        <div className="d-flex">
-                          <img src={"https://static.dotgg.gg/pokepocket/card/" + Trade.IdCard + ".webp"} alt="" style={{ height: 96 }} />
-                          <div className="flex-grow-1 ms-2">
-                            <div className="badge bg-primary">{Cards[Trade.IdCard][1]}</div>
-                            <div className="fw-bold text-truncate">{Cards[Trade.IdCard][0]}</div>
-                            <div className="d-flex flex-wrap gap-1">
-
-                              <div className='d-flex align-items-center'>
-                                <div className="fw-bold small" style={{ width: 24, height: 24 }}>
-                                  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
-                                    <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 17h12M4 17l3.5-3.5M4 17l3.5 3.5M7 7h13m0 0l-3.5-3.5M20 7l-3.5 3.5"></path>
-                                  </svg>
-                                </div>
-                              </div>
-                              {Trade.Offers.slice(0, 3).map((IdCard: string) => {
-                                return (
-                                  <img src={"https://static.dotgg.gg/pokepocket/card/" + IdCard + ".webp"} alt="" key={IdCard} style={{ height: 48 }} />
-                                )
-                              })}
-                              {Trade.Offers.length > 3 && (
-                                <div className='d-flex align-items-center'>
-                                  <div className="fw-bold small" style={{ width: 24, height: 24 }}>
-                                    +{Trade.Offers.length - 3}
-                                  </div>
-                                </div>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-
-                      </div>
-                    )
-                  })}
-                </div>
-              )}
-            </div>
-          </div>
-
-          <pre>{JSON.stringify(Trades, null, 2)}</pre>
+      <div className="card mb-3">
+        <div className="card-body pt-2">
+          <div className="text-muted small ps-2">I want</div>
+          <input type="text" className="form-control rounded-3" placeholder="Mewtwo"
+            value={Search} onChange={(e) => setSearch(e.target.value)} />
         </div>
       </div>
+
+      {TradesFiltered.length === 0 ? (
+        <div className="text-muted small text-center">No trades found</div>
+      ) : (
+        <div className="d-flex flex-column">
+          <div className="d-flex justify-content-between">
+            <div className="text-success h6 w-50 text-center">Receive</div>
+            <div className="opacity-25">
+              <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
+                <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 17h12M4 17l3.5-3.5M4 17l3.5 3.5M7 7h13m0 0l-3.5-3.5M20 7l-3.5 3.5"></path>
+              </svg>
+            </div>
+            <div className="text-danger h6 w-50 text-center">Send</div>
+          </div>
+          {TradesFiltered.map((Trade: any) => {
+            const OffersFiltered = Trade.Offers.filter((IdCard: string) => {
+              if (Search.trim().length) {
+                return Cards[IdCard][0].toLowerCase().includes(Search.toLowerCase());
+              }
+              return true;
+            });
+            return (
+              <div key={Trade.IdTrade} className="d-flex overflow-hidden">
+                <div className="w-50 pb-2">
+                  <div className="card h-100 rounded-end-0 border-end" style={{ backgroundColor: '#223b40' }}>
+                    <div className="card-body p-2">
+
+                      <div className="d-flex flex-wrap gap-1">
+                        {OffersFiltered.length === 1 ? (
+                          <div className="d-flex">
+                            <img src={"https://static.dotgg.gg/pokepocket/card/" + OffersFiltered[0] + ".webp"} alt="" style={{ height: 96 }}
+                              className="me-2" />
+                            <div className="flex-grow-1">
+                              <div className="badge bg-primary">{Cards[OffersFiltered[0]][1]}</div>
+                              <div className="fw-bold text-truncate">{Cards[OffersFiltered[0]][0]}</div>
+                              <div className="small">#{OffersFiltered[0]}</div>
+                            </div>
+                          </div>
+                        ) : (
+                          OffersFiltered.map((IdCard: string) => {
+                            const Height = OffersFiltered.length > 1 ? (OffersFiltered.length > 3 ? 46 : 64) : 96;
+                            return (
+                              <img src={"https://static.dotgg.gg/pokepocket/card/" + IdCard + ".webp"} alt="" key={IdCard} style={{ height: Height }} />
+                            )
+                          })
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-50 pb-2">
+                  <div className="card h-100 rounded-start-0" style={{ backgroundColor: '#3f2f3d' }}>
+                    <div className="card-body p-2 d-flex">
+                      <img src={"https://static.dotgg.gg/pokepocket/card/" + Trade.IdCard + ".webp"} alt="" style={{ height: 96 }}
+                        className="img-fluid d-block" />
+                      <div className="flex-grow-1 ms-2">
+                        <div className="badge bg-primary">{Cards[Trade.IdCard][1]}</div>
+                        <div className="fw-bold text-truncate">{Cards[Trade.IdCard][0]}</div>
+                        <div className="small">#{Trade.IdCard}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   ) : (
     <div className="d-flex justify-content-center align-items-center h-100">
